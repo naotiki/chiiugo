5896a0b70ac877a4497818d38af0ebc466db69ce
Happy birthday to sus <117884839+Nimono-sleep-well@users.noreply.github.com>
2023-05-14 14:55:22 +0900

    停止後の行動(雛形)

--- src/main/kotlin/AnimationType.kt
+++ src/main/kotlin/AnimationType.kt
@@ -1,13 +1,13 @@
 import androidx.compose.animation.core.tween
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.WindowPosition
 import kotlinx.coroutines.delay
+import org.jetbrains.skia.ColorFilter
 import kotlin.random.Random
 
 enum class AnimationType(val action: suspend WindowAnimatable.() -> Unit) {
     Test({
 
-        kotlin.repeat(200){
+        kotlin.repeat(200) {
             animateTo(
                 this.value.copy(
                     x = (Random.nextFloat() * ScreenSize.widthDp).dp,
@@ -18,6 +18,14 @@ enum class AnimationType(val action: suspend WindowAnimatable.() -> Unit) {
                 )
             )
             delay(500)
+            val range = (0..2).random()
+            if (range == 0){
+                kotlin.io.println("test")
+            }else if(range == 1){
+                kotlin.io.println("test2")
+            }else{
+                kotlin.io.println("test3")
+            }//静止した後ランダムで(行動)or(何もしない)をする
         }
     })
 }
